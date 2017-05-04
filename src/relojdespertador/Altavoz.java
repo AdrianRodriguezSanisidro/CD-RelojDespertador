a4e9f7e36f23cbe9e811fc2fb1b4c6670dc1a93a
Adrian Rodriguez Sanisidro <adrysanisidro1@gmail.com>
2017-05-04 10:01:01 +0200

    Method for start the music

---
+++ src/relojdespertador/Altavoz.java
@@ -0,0 +1,20 @@
+/*
+Have the melodi´s methods
+ */
+package relojdespertador;
+
+/**
+ *
+ * @author Adry
+ */
+public class Altavoz {
+    String melodia;
+    static boolean ringON;
+    /**
+     * The melody start
+     */
+    public void playSound(){
+        
+    }
+    }
+    
